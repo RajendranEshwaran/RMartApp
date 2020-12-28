bff143efde80c04cafefe366f10d77f7392171a5
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-12-28 15:32:13 -0500

    Initial Commit

---
+++ RMartApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RMartApp
+//
+//  Created by gomathi saminathan on 12/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
